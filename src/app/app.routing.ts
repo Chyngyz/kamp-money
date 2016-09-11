ec9f5cc83eebd0bd3f63e777a53957e53352d7f3
Chyngyz Arystan uulu <camphosis@gmail.com>
2016-09-12 01:47:36 +0600

    Main header done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 import { SigninComponent } from './signin/signin.component';
 import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'signin', component: SigninComponent},
-  { path: 'create', component: CreateComponent}
+  { path: 'signin', component: SigninComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'main', component: MainComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
