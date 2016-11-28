39b01578b2d0e75f171ea2b73572ad217d17ca4d
Andrey Pohilko <anoxis@gmail.com>
2016-11-28 11:25:48 +0600

    new build script for ci

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+set -ex
+
+function SetVariables {
+  source "$( dirname "${BASH_SOURCE[0]}" )/settings.sh"
+  export remote_demo_host=kampmoney@demo.kampmoney
+  export VERSION_NUMBER=0.1.${1}
+  export NVM_DIR="/var/lib/jenkins/.nvm"
+}
+
+function PutVersionNumber {
+  set +x
+  set -x
+  echo ${1} > ${HOME_FOLDER}/version.txt
+}
+
+
+function BuildServices {
+  # build mobile-app separately using npm
+  # build angular was disabled until Chyngyz fixes it
+  [ -s "$NVM_DIR/nvm.sh" ] 
+  source "$NVM_DIR/nvm.sh" 
+  npm run clean-install  # download dependencies if any
+  npm run build  # build the angular app
+}
+
+SetVariables ${1}
+PutVersionNumber ${VERSION_NUMBER}
+BuildServices
